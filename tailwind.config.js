d20be609ef24f292032d6f93f83efe11990e34b4
fatmaemiirr <fatmaemiirr@gmail.com>
2025-01-13 23:15:13 +0300

    Changes have been made

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container:{
+      center: true,
+      screens:{  
+        lg: "1140px",
+        xl: "1140px",
+        '2xl': "1140px",}
+
+    },
+    extend: {
+      fontFamily:{
+        gemunu: ['Gemunu Libre' , 'sans-serif'],
+        open: ('Open Sans', 'sans-serif'),
+
+      },
+      colors:{
+        'trend-red' : '#BC1A45',
+        'trend-yellow': '#FFD369',
+        'trend-grey': '#DDDDDD',
+        'trend-white': '#F7F7F7',
+      },
+      spacing:{
+        128: '32rem', 
+      }
+    },
   },
   plugins: [],
-}
+};
 
